cb8e08cf7d7d4a939f5d3d2a58a37ddacf6d22d4
Raymond Cvoz <sranchvook@outlook.com>
2021-07-11 19:26:52 +0800

    almost finished

--- agenda/src/main/java/com/raymond/agenda/ui/timer/TimerFragment.java
+++ agenda/src/main/java/com/raymond/agenda/ui/timer/TimerFragment.java
@@ -1,16 +1,14 @@
 package com.raymond.agenda.ui.timer;
 
-import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.SyncAdapterType;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
-import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,110 +18,159 @@ import androidx.fragment.app.Fragment;
 
 import com.raymond.agenda.databinding.FragmentTimerBinding;
 
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class TimerFragment extends Fragment
 {
-    private int bindStatus = 0;
-    private int timerRunning = 0;
+    TimerService timerService;
+    boolean mBound = false;
+
     private FragmentTimerBinding binding;
-    private TimerService.TimerBinder timerBinder;
-    private Timer timer;
-    private int secondLeft;
 
-    private ServiceConnection serviceConnection = new ServiceConnection()
+    int calcRemain()
     {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service)
+        String hours = binding.inputHours.getText().toString();
+        if (hours.equals(""))
         {
-            timerBinder = (TimerService.TimerBinder) service;
-            String hours = binding.inputHours.getText().toString();
-            if (hours.equals(""))
-            {
-                hours = "0";
-            }
-            String minutes = binding.inputMinutes.getText().toString();
-            if (minutes.equals(""))
-            {
-                minutes = "0";
-            }
-            String second = binding.inputSeconds.getText().toString();
-            if (second.equals(""))
-            {
-                second = "0";
-            }
-            Integer htos = Integer.parseInt(hours) * 3600;
-            Integer mtos = Integer.parseInt(minutes) * 60;
-            Integer sec = Integer.parseInt(second);
-            Integer result = htos + mtos + sec;
-            timerBinder.setRemaining(result);
-            timerBinder.count();
+            hours = "0";
         }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name)
+        String minutes = binding.inputMinutes.getText().toString();
+        if (minutes.equals(""))
         {
-
+            minutes = "0";
         }
-    };
+        String second = binding.inputSeconds.getText().toString();
+        if (second.equals(""))
+        {
+            second = "0";
+        }
+        Integer htos = Integer.parseInt(hours) * 3600;
+        Integer mtos = Integer.parseInt(minutes) * 60;
+        Integer sec = Integer.parseInt(second);
+        Integer result = htos + mtos + sec;
+        return result;
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         binding = FragmentTimerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        return root;
+    }
 
-        timer = new Timer();
-        TimerTask timerTask = new TimerTask()
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Intent intent = new Intent(getActivity(),TimerService.class);
+        getActivity().bindService(intent,connection,Context.BIND_AUTO_CREATE);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        Runnable runnable = new Runnable()
         {
-            @SuppressLint("SetTextI18n")
             @Override
             public void run()
             {
-                if (bindStatus == 1)
-                {
-                    secondLeft = timerBinder.getRemaining();
-                    binding.secondsLeft.setText(secondLeft + "");
-                }
+                int remain = timerService.getRemaining();
+
+                int hour = remain / 3600;
+                remain -= hour * 3600;
+                int minute = remain / 60;
+                remain -= minute * 60;
+                int second = remain;
+
+                binding.hoursLeft.setText(hour + "");
+                binding.minutesLeft.setText(minute + "");
+                binding.secondsLeft.setText(second + "");
+
+                handler.postDelayed(this,1000);
             }
         };
+        handler.postDelayed(runnable,300);
 
-        timer.schedule(timerTask, 0, 1000);
-        timerRunning = 1;
+
         binding.timerStart.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(getContext(), TimerService.class);
-                getContext().startService(intent);
-                getContext().bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
-                bindStatus = 1;
+                String hourS = binding.inputHours.getText().toString();
+                if(hourS.equals(""))
+                {
+                    hourS = "0";
+                }
+                String minuteS = binding.inputMinutes.getText().toString();
+                if(minuteS.equals(""))
+                {
+                    minuteS = "0";
+                }
+                String secondS = binding.inputSeconds.getText().toString();
+                if(secondS.equals(""))
+                {
+                    secondS = "0";
+                }
+
+
+                int hour = Integer.parseInt(hourS);
+                int minute = Integer.parseInt(minuteS);
+                int second = Integer.parseInt(secondS);
+
+                if(mBound)
+                {
+                    timerService.setRemaining(hour * 3600 + minute * 60 + second);
+                    timerService.beginCount();
+                    binding.timerStart.setEnabled(false);
+                    binding.timerStop.setEnabled(true);
+                }
             }
         });
-
         binding.timerStop.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                if (bindStatus == 1)
+                if(mBound)
                 {
-                    Intent intent = new Intent(getContext(), TimerService.class);
-                    getContext().unbindService(serviceConnection);
-                    getContext().stopService(intent);
-                    timerBinder.countDone();
-                    bindStatus = 0;
+                    timerService.stopCount();
+                    binding.timerStart.setEnabled(true);
+                    binding.timerStop.setEnabled(false);
                 }
             }
         });
-        return root;
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+
     }
 
     @Override
     public void onDestroyView()
     {
+
         super.onDestroyView();
-        binding = null;
     }
+
+    private ServiceConnection connection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            TimerService.LocalBinder binder = (TimerService.LocalBinder)service;
+            timerService = binder.getService();
+            mBound = true;
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            mBound = false;
+        }
+    };
 }
--- agenda/src/main/java/com/raymond/agenda/ui/timer/TimerService.java
+++ agenda/src/main/java/com/raymond/agenda/ui/timer/TimerService.java
@@ -1,72 +1,52 @@
 package com.raymond.agenda.ui.timer;
 
-import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
-import android.util.Log;
-import android.widget.Toast;
+import android.os.Looper;
 
-import androidx.media.app.NotificationCompat;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import com.raymond.agenda.MainFrame;
+import com.raymond.agenda.R;
+import com.raymond.agenda.databinding.FragmentTimerBinding;
 
 public class TimerService extends Service
 {
+
+    private static final String CHANNEL_DEFAULT_IMPORTANCE = "service";
+    private static final String CHANNEL_ID = "default";
     private int remaining;
-    private TimerBinder timerBinder = new TimerBinder(remaining);
-
-    class TimerBinder extends Binder
+    private final IBinder binder = new LocalBinder();
+    private int signal;
+    private Handler handler;
+    private Runnable runnable;
+    private int kill = 0;
+    private FragmentTimerBinding binding;
+    public class LocalBinder extends Binder
     {
-        private int remaining;
-        public TimerBinder(int remaining)
+        TimerService getService()
         {
-            this.remaining = remaining;
-        }
-
-        public int getRemaining()
-        {
-            return remaining;
-        }
-
-        public void setRemaining(int remaining)
-        {
-            this.remaining = remaining;
-        }
-
-        Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask()
-        {
-
-            @Override
-            public void run()
-            {
-                remaining--;
-                Log.i("count", "run: ");
-            }
-        };
-
-        void count()
-        {
-            timer.schedule(timerTask,0,1000);
-        }
-
-        void countDone()
-        {
-            timer.cancel();
-            timerTask.cancel();
-            timer = null;
-            timerTask = null;
+            System.out.println("localBinder constructed");
+            return TimerService.this;
         }
     }
 
     @Override
-    public void onCreate()
+    public IBinder onBind(Intent intent)
     {
-        super.onCreate();
+        // TODO: Return the communication channel to the service.
+        return binder;
     }
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
@@ -74,16 +54,78 @@ public class TimerService extends Service
         return super.onStartCommand(intent, flags, startId);
     }
 
-    @Override
-    public void onDestroy()
+    public void setRemaining(int remaining)
     {
-        super.onDestroy();
+        this.remaining = remaining;
     }
 
-    @Override
-    public IBinder onBind(Intent intent)
+    public int getRemaining()
     {
-        // TODO: Return the communication channel to the service.
-        return timerBinder;
+        return remaining;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    void showNotification()
+    {
+        final String CHANNEL_ID = "channel_id_1";
+        final String CHANNEL_NAME = "channel_name_1";
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+
+        Intent intent = new Intent(this, MainFrame.class);
+        intent.putExtra("navTo",3);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+
+        builder.setSmallIcon(R.drawable.ic_dashboard_black_24dp)
+                .setContentTitle("定时事件")
+                .setContentText("时间到了")
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true).build();
+
+        notificationManager.notify(1, builder.build());
+    }
+
+
+    public void beginCount()
+    {
+        kill = 0;
+        handler = new Handler(Looper.getMainLooper());
+        runnable = new Runnable()
+        {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void run()
+            {
+                if(kill == 0)
+                {
+                    if (remaining > 0)
+                    {
+                        remaining--;
+                    } else
+                    {
+                        showNotification();
+                        stopCount();
+                    }
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1000);
+    }
+
+    public void stopCount()
+    {
+        kill = 1;
+        handler.removeCallbacks(runnable);
     }
 }
